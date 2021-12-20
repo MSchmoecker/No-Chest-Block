dc3eb583d466109f7e90bd5e28d37eb938e29d1e
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2021-12-20 11:04:45 +0100

    removed generating Unity files from this project

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 ModName="ChestFix"
-ModNameUnity="UnityChestFix"
 
 # function for xml reading
 read_dom () {
@@ -30,34 +29,8 @@ fi
 PluginFolder="$BepInExFolder/plugins"
 ModDir="$PluginFolder/$ModName"
 
-# copy to unity
-mkdir -p "$ModNameUnity/Assets/Assemblies"
-mkdir -p "$ModNameUnity/AssetBundles/StandaloneWindows"
-
-cp "$ModName/bin/Debug/$ModName.dll" "$ModNameUnity/Assets/Assemblies"
-
-cp "$BepInExFolder/core/BepInEx.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$BepInExFolder/core/0Harmony.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$BepInExFolder/core/Mono.Cecil.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$BepInExFolder/core/MonoMod.Utils.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$BepInExFolder/core/MonoMod.RuntimeDetour.dll" "$ModNameUnity/Assets/Assemblies"
-[ -f "$PluginFolder/ValheimModding-Jotunn/Jotunn.dll" ] && cp "$PluginFolder/ValheimModding-Jotunn/Jotunn.dll" "$ModNameUnity/Assets/Assemblies"
-[ -f "$PluginFolder/Jotunn/Jotunn.dll" ] && cp "$PluginFolder/Jotunn/Jotunn.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_valheim.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_utils.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_postprocessing.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_sunshafts.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_guiutils.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_steamworks.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_googleanalytics.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_valheim.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_utils.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_postprocessing.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_sunshafts.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_guiutils.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_steamworks.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_googleanalytics.dll" "$ModNameUnity/Assets/Assemblies"
-echo Coping to: "$ModDir"
+echo Coping...
+echo "$ModDir"
 
 # copy content
 mkdir -p "$ModDir"
