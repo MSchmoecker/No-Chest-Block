19827d5efa9f2770f80e3fab693257c034f638a0
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-01-12 22:01:50 +0100

    fix: correctly return true/false for AddItem if intercepted

--- MultiUserChest/Patches/InventoryPatch.cs
+++ MultiUserChest/Patches/InventoryPatch.cs
@@ -10,10 +10,10 @@ namespace MultiUserChest.Patches {
         [HarmonyPatch(typeof(Inventory), nameof(Inventory.AddItem), typeof(ItemDrop.ItemData), typeof(int), typeof(int), typeof(int))]
         [HarmonyPrefix]
         public static bool AddItem1Prefix(Inventory __instance, ItemDrop.ItemData item, int amount, int x, int y, ref bool __result) {
-            bool intercepted = InterceptAddItem(__instance, item, amount, new Vector2i(x, y));
+            bool intercepted = InterceptAddItem(__instance, item, amount, new Vector2i(x, y), out bool successfulAdded);
 
             if (intercepted) {
-                __result = true;
+                __result = successfulAdded;
                 return false;
             }
 
@@ -23,34 +23,37 @@ namespace MultiUserChest.Patches {
         [HarmonyPatch(typeof(Inventory), nameof(Inventory.AddItem), typeof(ItemDrop.ItemData))]
         [HarmonyPrefix]
         public static bool AddItem2Prefix(Inventory __instance, ItemDrop.ItemData item, ref bool __result) {
-            bool intercepted = InterceptAddItem(__instance, item, item.m_stack, new Vector2i(-1, -1));
+            bool intercepted = InterceptAddItem(__instance, item, item.m_stack, new Vector2i(-1, -1), out bool successfulAdded);
 
             if (intercepted) {
-                __result = true;
+                __result = successfulAdded;
                 return false;
             }
 
             return true;
         }
 
-        private static bool InterceptAddItem(Inventory inventory, ItemDrop.ItemData item, int amount, Vector2i pos) {
+        private static bool InterceptAddItem(Inventory inventory, ItemDrop.ItemData item, int amount, Vector2i pos, out bool successfulAdded) {
             InventoryOwner from = InventoryOwner.GetInventoryObjectOfItem(item);
             InventoryOwner to = InventoryOwner.GetInventoryObject(inventory);
 
             if (from == null || !from.IsValid() || to == null || !to.IsValid()) {
+                successfulAdded = false;
                 return false;
             }
 
             if (from.ZNetView.IsOwner() && !to.ZNetView.IsOwner()) {
                 if (to is ContainerInventoryOwner toContainer) {
-                    toContainer.Container.AddItemToChest(item, from.Inventory, pos, from.ZNetView.GetZDO().m_uid, amount, true);
+                    RequestAdd requestAdd =  toContainer.Container.AddItemToChest(item, from.Inventory, pos, from.ZNetView.GetZDO().m_uid, amount, true);
+                    successfulAdded = requestAdd.dragAmount == amount;
                     return true;
                 }
             }
 
             if (!from.ZNetView.IsOwner() && to.ZNetView.IsOwner()) {
                 if (from is ContainerInventoryOwner fromContainer) {
-                    fromContainer.Container.RemoveItemFromChest(item, to.Inventory, pos, to.ZNetView.GetZDO().m_uid, amount, to.Inventory.GetItemAt(pos.x, pos.y));
+                    RequestRemove requestRemove = fromContainer.Container.RemoveItemFromChest(item, to.Inventory, pos, to.ZNetView.GetZDO().m_uid, amount, to.Inventory.GetItemAt(pos.x, pos.y));
+                    successfulAdded = requestRemove.dragAmount == amount;
                     return true;
                 }
             }
@@ -58,10 +61,12 @@ namespace MultiUserChest.Patches {
             if (!from.ZNetView.IsOwner() && !to.ZNetView.IsOwner()) {
                 if (from is ContainerInventoryOwner fromContainer && to is ContainerInventoryOwner toContainer && fromContainer.Container == toContainer.Container) {
                     fromContainer.Container.MoveItemInChest(item.m_gridPos, pos, amount);
+                    successfulAdded = true;
                     return true;
                 }
             }
 
+            successfulAdded = false;
             return false;
         }
 
