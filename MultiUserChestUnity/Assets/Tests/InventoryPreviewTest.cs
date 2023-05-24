ad5c823a8c42071c7e00093165a71afae9b02414
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-05-24 21:55:41 +0200

    chore: more preview tests

--- MultiUserChestUnity/Assets/Tests/InventoryPreviewTest.cs
+++ MultiUserChestUnity/Assets/Tests/InventoryPreviewTest.cs
@@ -89,6 +89,27 @@ namespace UnitTests {
         }
 
         [Test]
+        public void AddRequest_FastMove() {
+            player.CreateItem("item A", 5, 0, 0);
+
+            RequestChestAdd request = new RequestChestAdd(new Vector2i(-1, -1), 5, player.GetItemAt(0, 0), player, chest);
+            InventoryPreview.AddPackage(request);
+            player.RemoveItem(player.GetItemAt(0, 0));
+
+            bool hasPlayerChanges = InventoryPreview.GetChanges(player, out SlotPreview playerPreview);
+            bool hasChestChanges = InventoryPreview.GetChanges(chest, out SlotPreview chestPreview);
+
+            Assert.IsFalse(hasPlayerChanges, "Player should not have changes");
+
+            Assert.IsTrue(hasChestChanges, "Chest should have changes");
+            TestSlotPreview(chest, chestPreview, new ExpectedSlotPreview {
+                hasChanges = true,
+                pos = new Vector2i(0, 0),
+                item = new TestItem("item A", 5, new Vector2i(0, 0))
+            });
+        }
+
+        [Test]
         public void RemoveRequest_EmptySlot() {
             chest.CreateItem("item A", 5, 0, 1);
             RequestChestRemove request = new RequestChestRemove(new Vector2i(0, 1), new Vector2i(1, 0), 5, null, chest, player);
@@ -164,6 +185,114 @@ namespace UnitTests {
         }
 
         [Test]
+        public void RemoveRequest_Stack_Twice() {
+            chest.CreateItem("item A", 10, 0, 1);
+
+            RequestChestRemove request = new RequestChestRemove(new Vector2i(0, 1), new Vector2i(0, 0), 3, null, chest, player);
+            InventoryPreview.AddPackage(request);
+
+            request = new RequestChestRemove(new Vector2i(0, 1), new Vector2i(0, 0), 3, null, chest, player);
+            InventoryPreview.AddPackage(request);
+
+            bool hasPlayerChanges = InventoryPreview.GetChanges(player, out SlotPreview playerPreview);
+            bool hasChestChanges = InventoryPreview.GetChanges(chest, out SlotPreview chestPreview);
+
+            Assert.IsTrue(hasPlayerChanges, "Player should have changes");
+            TestSlotPreview(player, playerPreview, new ExpectedSlotPreview {
+                hasChanges = true,
+                pos = new Vector2i(0, 0),
+                item = new TestItem("item A", 6, new Vector2i(0, 0))
+            });
+
+            Assert.IsTrue(hasChestChanges, "Chest should have changes");
+            TestSlotPreview(chest, chestPreview, new ExpectedSlotPreview {
+                hasChanges = true,
+                pos = new Vector2i(0, 1),
+                item = new TestItem("item A", 4, new Vector2i(0, 1))
+            });
+        }
+
+        [Test]
+        public void RemoveRequest_FastMove_NoStack() {
+            player.CreateItem("item B", 5, 0, 0);
+            chest.CreateItem("item A", 5, 0, 0);
+
+            RequestChestRemove request = new RequestChestRemove(new Vector2i(0, 0), new Vector2i(-1, -1), 5, null, chest, player);
+            InventoryPreview.AddPackage(request);
+
+            bool hasPlayerChanges = InventoryPreview.GetChanges(player, out SlotPreview playerPreview);
+            bool hasChestChanges = InventoryPreview.GetChanges(chest, out SlotPreview chestPreview);
+
+            Assert.IsTrue(hasPlayerChanges, "Player should have changes");
+            TestSlotPreview(player, playerPreview, new ExpectedSlotPreview {
+                hasChanges = true,
+                pos = new Vector2i(1, 0),
+                item = new TestItem("item A", 5, new Vector2i(1, 0))
+            });
+
+            Assert.IsTrue(hasChestChanges, "Chest should have changes");
+            TestSlotPreview(chest, chestPreview, new ExpectedSlotPreview {
+                hasChanges = true,
+                pos = new Vector2i(0, 0),
+                item = null
+            });
+        }
+
+        [Test]
+        public void RemoveRequest_FastMove_Stack() {
+            player.CreateItem("item A", 5, 0, 1);
+            chest.CreateItem("item A", 5, 1, 0);
+
+            RequestChestRemove request = new RequestChestRemove(new Vector2i(1, 0), new Vector2i(-1, -1), 5, null, chest, player);
+            InventoryPreview.AddPackage(request);
+
+            bool hasPlayerChanges = InventoryPreview.GetChanges(player, out SlotPreview playerPreview);
+            bool hasChestChanges = InventoryPreview.GetChanges(chest, out SlotPreview chestPreview);
+
+            Assert.IsTrue(hasPlayerChanges, "Player should have changes");
+            TestSlotPreview(player, playerPreview, new ExpectedSlotPreview {
+                hasChanges = true,
+                pos = new Vector2i(0, 1),
+                item = new TestItem("item A", 10, new Vector2i(0, 1))
+            });
+
+            Assert.IsTrue(hasChestChanges, "Chest should have changes");
+            TestSlotPreview(chest, chestPreview, new ExpectedSlotPreview {
+                hasChanges = true,
+                pos = new Vector2i(1, 0),
+                item = null
+            });
+        }
+
+        [Test]
+        public void RequestRemove_FastMove_Split_Twice() {
+            chest.CreateItem("item A", 10, 0, 0);
+
+            RequestChestRemove request = new RequestChestRemove(new Vector2i(0, 0), new Vector2i(-1, -1), 5, null, chest, player);
+            InventoryPreview.AddPackage(request);
+
+            request = new RequestChestRemove(new Vector2i(0, 0), new Vector2i(-1, -1), 3, null, chest, player);
+            InventoryPreview.AddPackage(request);
+
+            bool hasChestChanges = InventoryPreview.GetChanges(chest, out SlotPreview chestPreview);
+            bool hasPlayerChanges = InventoryPreview.GetChanges(player, out SlotPreview playerPreview);
+
+            Assert.IsTrue(hasChestChanges, "Chest should have changes");
+            TestSlotPreview(chest, chestPreview, new ExpectedSlotPreview {
+                hasChanges = true,
+                pos = new Vector2i(0, 0),
+                item = new TestItem("item A", 2, new Vector2i(0, 0))
+            });
+
+            Assert.IsTrue(hasPlayerChanges, "Player should have changes");
+            TestSlotPreview(player, playerPreview, new ExpectedSlotPreview {
+                hasChanges = true,
+                pos = new Vector2i(0, 0),
+                item = new TestItem("item A", 8, new Vector2i(0, 0))
+            });
+        }
+
+        [Test]
         public void MoveRequest_EmptySlot() {
             chest.CreateItem("item A", 5, 0, 1);
 
@@ -181,6 +310,30 @@ namespace UnitTests {
                 hasChanges = true,
                 pos = new Vector2i(1, 0),
                 item = new TestItem("item A", 5, new Vector2i(1, 0))
+            });
+        }
+
+        [Test]
+        public void MoveRequest_Stack_Twice() {
+            chest.CreateItem("item A", 5, 0, 1);
+
+            RequestMove request = new RequestMove(chest.GetItemAt(0, 1), new Vector2i(1, 0), 3, chest);
+            InventoryPreview.AddPackage(request);
+
+            request = new RequestMove(chest.GetItemAt(0, 1), new Vector2i(1, 0), 1, chest);
+            InventoryPreview.AddPackage(request);
+
+            bool hasChestChanges = InventoryPreview.GetChanges(chest, out SlotPreview chestPreview);
+
+            Assert.IsTrue(hasChestChanges, "Chest should have changes");
+            TestSlotPreview(chest, chestPreview, new ExpectedSlotPreview {
+                hasChanges = true,
+                pos = new Vector2i(0, 1),
+                item = new TestItem("item A", 1, new Vector2i(0, 1))
+            }, new ExpectedSlotPreview {
+                hasChanges = true,
+                pos = new Vector2i(1, 0),
+                item = new TestItem("item A", 4, new Vector2i(1, 0))
             });
         }
 
